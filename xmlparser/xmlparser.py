15d30df6eba2b5e68b3c273d8c1d43be3508cd6b
Kenny <xiaozhong.wang@esgyn.cn>
2017-12-29 14:33:40 +0000

    modify for analyze the result generated by tscancode

--- xmlparser/xmlparser.py
+++ xmlparser/xmlparser.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
  
 import xml.sax
+import os
 import sys
 import pdb
  
 class XMLHandler(xml.sax.ContentHandler):
-   def __init__(self, cond):
-      self.Condition = cond
-      self.RowArray = []
+   def __init__(self):
       self.KeyArray = ["file", "line", "id", "subid", "severity", "msg", 
                        "web_identify", "content"]
       self.RuleArray = {
@@ -47,153 +46,182 @@ class XMLHandler(xml.sax.ContentHandler):
          "possibleUninitMemberInCtor":"Serious",
          
          # Warning sub types
-         "funcRetNull"              :"Warning",
-         "memleak"                  :"Warning",
-         "memleakOnRealloc"         :"Warning",
-         "resourceLeak"             :"Warning",
-         "deallocDealloc"           :"Warning",
-         "deallocuse"               :"Warning",
-         "mismatchAllocDealloc"     :"Warning",
-         "mismatchSize"             :"Warning",
-         "virtualDestructor"        :"Warning",
-         "incrementboolean"         :"Warning",
-         "moduloAlwaysTrueFalse"    :"Warning",
-         "SizeofForNumericParameter":"Warning",
-         "SizeofForArrayParameter"  :"Warning",
-         "SizeofForPointerSize"     :"Warning",
-         "sizeofsizeof"             :"Warning",
-         "UnsignedDivision"         :"Warning",
-         "NegativeBitwiseShift"     :"Warning",
-         "autovar"                  :"Warning",
-         "Danglingpointer"          :"Warning",
-         "NestedLoop"               :"Warning",
-         "unConditionalBreakinLoop" :"Warning",
-         "IfCondition"              :"Warning",
-         "strPlusChar"              :"Warning",
-         "bitwiseOnBoolean"         :"Warning",
-         "ComparisonOfBoolWithInt"  :"Warning",
-         "ComparisonOfBoolWithBool" :"Warning",
-         "ComparisonWithBool"       :"Warning",
-         "assignIf"                 :"Warning",
-         "oppositeInnerCondition"   :"Warning",
-         "incorrectLogicOperator"   :"Warning",
-         "clarifyCondition"         :"Warning",
-         "StlMissingComparison"     :"Warning",
-         "stlSortCheck"             :"Warning",
-         "SwitchNoBreakUP"          :"Warning",
-         "NoFirstCase"              :"Warning",
-         "RecursiveFunc"            :"Warning",
-         "UnintentionalOverflow"    :"Warning",
-         "duplicateBranch"          :"Warning",
-         "DuplicateExpression"      :"Warning",
-         "selfAssignment"           :"Warning",
-         "duplicateIf"              :"Warning",
-         "STLFindError"             :"Warning",
-         "unsafeFunctionUsage"      :"Warning",
-         "uninitMemberVar"          :"Warning",
+         "funcRetNull"               :"Warning",
+         "memleak"                   :"Warning",
+         "memleakOnRealloc"          :"Warning",
+         "resourceLeak"              :"Warning",
+         "deallocDealloc"            :"Warning",
+         "deallocuse"                :"Warning",
+         "mismatchAllocDealloc"      :"Warning",
+         "mismatchSize"              :"Warning",
+         "virtualDestructor"         :"Warning",
+         "incrementboolean"          :"Warning",
+         "moduloAlwaysTrueFalse"     :"Warning",
+         "SizeofForNumericParameter" :"Warning",
+         "SizeofForArrayParameter"   :"Warning",
+         "SizeofForPointerSize"      :"Warning",
+         "sizeofsizeof"              :"Warning",
+         "UnsignedDivision"          :"Warning",
+         "NegativeBitwiseShift"      :"Warning",
+         "autovar"                   :"Warning",
+         "Danglingpointer"           :"Warning",
+         "NestedLoop"                :"Warning",
+         "unConditionalBreakinLoop"  :"Warning",
+         "IfCondition"               :"Warning",
+         "strPlusChar"               :"Warning",
+         "bitwiseOnBoolean"          :"Warning",
+         "ComparisonOfBoolWithInt"   :"Warning",
+         "ComparisonOfBoolWithBool"  :"Warning",
+         "ComparisonWithBool"        :"Warning",
+         "assignIf"                  :"Warning",
+         "oppositeInnerCondition"    :"Warning",
+         "incorrectLogicOperator"    :"Warning",
+         "clarifyCondition"          :"Warning",
+         "StlMissingComparison"      :"Warning",
+         "stlSortCheck"              :"Warning",
+         "SwitchNoBreakUP"           :"Warning",
+         "NoFirstCase"               :"Warning",
+         "RecursiveFunc"             :"Warning",
+         "UnintentionalOverflow"     :"Warning",
+         "duplicateBranch"           :"Warning",
+         "DuplicateExpression"       :"Warning",
+         "selfAssignment"            :"Warning",
+         "duplicateIf"               :"Warning",
+         "STLFindError"              :"Warning",
+         "unsafeFunctionUsage"       :"Warning",
+         "uninitMemberVar"           :"Warning",
 
          # Information sub types
-         "PreciseComparison"        :"Information",
-         "Unsignedlessthanzero"     :"Information",
-         "assignmentInAssert"       :"Information",
-         "suspiciousSemicolon"      :"Information",
-         "SuspiciousPriority"       :"Information",
-         "FuncReturn"               :"Information",
-         "BoolFuncReturn"           :"Information",
-         "SwitchNoDefault"          :"Information",
-         "SignedUnsignedMixed"      :"Information",
-         "redundantCondition"       :"Information"
+         "PreciseComparison"         :"Information",
+         "Unsignedlessthanzero"      :"Information",
+         "assignmentInAssert"        :"Information",
+         "suspiciousSemicolon"       :"Information",
+         "SuspiciousPriority"        :"Information",
+         "FuncReturn"                :"Information",
+         "BoolFuncReturn"            :"Information",
+         "SwitchNoDefault"           :"Information",
+         "SignedUnsignedMixed"       :"Information",
+         "redundantCondition"        :"Information"
          }
  
-   def startElement(self, tag, attrs):
-      if tag == "error":
-         # pdb.set_trace() 
+   def readfile(self, srcfilepath):
+      # read srouce file
+      srcfileobj = open(srcfilepath, 'r')
+      fsize = os.path.getsize(srcfilepath)
+      filebuf = srcfileobj.read(fsize);
+      srcfileobj.close()
+
+      return filebuf
+
+   def analyzeRowBuffer(self, row, length):
+      attrs = {}
+      # strstat [0: scan key, 1: scan key end, 2: string start, 3: string end]
+      strstat = 0 
+      curoff = 0
+      value = ""
+      # pdb.set_trace()
+      for off in range(0, length) : 
+         if row[off] == '=' and strstat == 0:
+            key = row[curoff : off]
+            curoff = off + 1
+            strstat = 1
+         elif row[off] == '"':
+            if strstat == 1:
+               curoff = off
+               strstat = 2
+            elif strstat == 2:
+               value = row[curoff+1 : off]
+               attrs[key] = value
+               strstat = 3
+         elif strstat == 3 and row[off] != ' ':
+            strstat = 0
+            curoff = off
+
+      return attrs
+               
+
+   def analyzeBuffer(self, filebuf, level):
+      start = filebuf.find("<error ") + 7
+      end = filebuf.find("</results>")
+
+      errorbuf = filebuf[start : end]
+      errors = errorbuf.split("<error ")
+
+      results = []
+
+      # pdb.set_trace()
+      for error in errors :
+         rowstart = error.find("file=")
+         rowend = error.find("/>")
+         row = error[rowstart : rowend]
+         length = rowend - rowstart
+         attrs = self.analyzeRowBuffer(row, length)
          subtype = attrs["subid"]
-         currule = self.RuleArray[subtype]
-         if currule == self.Condition :
+         curlevel = self.RuleArray[subtype]
+         if curlevel == level :
             row = {}
             for key in self.KeyArray:
                row[key] = attrs[key]
-            # print "====row====:", row
-            self.RowArray += [row]
+            results += [row]
+         
 
-   def outputFile(self, filepath="result.xml"):
-      file_object = open(filepath, 'w')
+      return results
 
-      file_object.write('<?xml version="1.0" encoding="UTF-8"?>\n<results>\n\n');
-
+   def outputFile(self, results, descfilepath):
+      descfileobj = open(descfilepath, 'w')
+      descfileobj.write('<?xml version="1.0" encoding="UTF-8"?>\n<results>\n\n');
       # pdb.set_trace() 
-      for row in self.RowArray:
+      for row in results:
          errorstring = "<error "
          for key in self.KeyArray:
             errorstring += key + '="' + row[key] + '" '
          errorstring += '/>\n\n'
 
-         file_object.write(errorstring)
+         descfileobj.write(errorstring)
 
-      file_object.write("\n</results>\n");
+      descfileobj.write("\n</results>\n");
 
-      file_object.close( )
- 
+      descfileobj.close( )
+      
+
+   def analyze(self, srcfilepath, level):
+      filebuf = self.readfile(srcfilepath)
+
+      results = self.analyzeBuffer(filebuf, level)
+
+      self.outputFile(results, level + "_" + srcfilepath)
+
 def helper(command):
    print """XMLFilter - A tool for xml filter
 
 Syntax:
-    %s {-v | -h | -f filter} [files]
+    %s [ -v | -h | files ]
 
 Options:
-    -f filter      Spicial filter condition
     -h
     --help         Show this help text
     -v
     --version      Show the version
-
-Filter:
-    Critical
-    Serious
-    Warning
-    Information
-    Style
 """ % (command)
 
 if ( __name__ == "__main__"):
    argv = sys.argv
    argc = len(sys.argv)
 
-   # pdb.set_trace() 
-   if argc == 2 and (argv[1] == "-v" or argv[1] == "--version"):
+   if argc != 2 or argv[1] == "-h" or argv[1] == "--help":
+      helper(argv[0])
+      sys.exit(0)
+
+   if argv[1] == "-v" or argv[1] == "--version":
       print "XMLFilter version V0.0.1"
       sys.exit(0)
-   elif argc == 2 and (argv[1] == "-h" or argv[1] == "--help"):
-      helper(argv[0])
-      sys.exit(0)
-   elif argc != 4:
-      print "input parameter error."
-      helper(argv[0])
-      sys.exit(1)
 
-   fconf = argv[1]
-   cond = argv[2]
-   filepath = argv[3]
-   if fconf != "-f":
-      print "input parameter error."
-      helper(argv[0])
-      sys.exit(1)
+   filepath = argv[1]
+   levels = ["Critical", "Serious", "Warning", "Information", "Style"]
 
-   #  Critical, Serious, Warning, Information, Style
-   if cond != "Critical" and cond != "Serious" and cond != "Warning" \
-          and cond != "Information" and cond != "Style" :
-      print "input filter parameter error."
-      helper(argv[0])
-      sys.exit(1)
-   
-   parser = xml.sax.make_parser()
-   parser.setFeature(xml.sax.handler.feature_namespaces, 0)
- 
-   Handler = XMLHandler(cond)
-   parser.setContentHandler(Handler)
-   
-   parser.parse(filepath)
-   Handler.outputFile()
+   Handler = XMLHandler()
+   # pdb.set_trace() 
+   for level in levels :
+      Handler.analyze(filepath, level)
 
    sys.exit(0)
